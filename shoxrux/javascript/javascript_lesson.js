e702efbc3c82ed38722e5970d7858bbacf16fa36
Shoxrux71 <amirjonovdamin@gmail.com>
2024-02-20 18:46:28 +0500

    new

--- shoxrux/javascript/javascript_lesson.js
+++ shoxrux/javascript/javascript_lesson.js
@@ -15,9 +15,13 @@
 
 
 const button = document.getElementById("button-id");
+
 button.addEventListener("click", updateSudentName);
+
 function updateSudentName() {
+
     var studentName = prompt("学生の名前を入力して下さい");
+
     button.style.color = 'green';
     button.textContent = `学生の 名前は: ${studentName}`;
 }
