81f647fa1e1b561b7065525a2c6ae3ac301be319
zeds <tom.zed39@gmail.com>
2024-02-28 22:42:14 +0700

    calc

--- shoxrux/calcWeb/calc.js
+++ shoxrux/calcWeb/calc.js
@@ -19,136 +19,138 @@ let btn_minus = document.querySelector(".btn_minus");
 let btn_multi = document.querySelector(".btn_multi");
 let btn_div = document.querySelector(".btn_div");
 let btn_eq = document.querySelector(".btn_eq");
-let btn_point = document.querySelector(".point");
+let btn_clear = document.querySelector(".clear");
+let btn_plus_minus = document.querySelector(".plus_minus");
+let btn_percent = document.querySelector(".percent");
 
-x.style.background = "red";
+x.style.background = "#A8AAFF";
 
 x.addEventListener("click", function () {
-    x.style.background = "#A8AAFF";
-    y.style.background = "transparent";
-    select = "x";
+	x.style.background = "#A8AAFF";
+	y.style.background = "transparent";
+	select = "x";
 });
 y.addEventListener("click", function () {
-    x.style.background = "transparent";
-    y.style.background = "#A8AAFF";
-    select = "y";
+	x.style.background = "transparent";
+	y.style.background = "#A8AAFF";
+	select = "y";
 });
 
 btn_0.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML !== "x") {
-            x.innerHTML = x.innerHTML + "0";
-        }
-    } else {
-        if (y.innerHTML !== "y") {
-            y.innerHTML = y.innerHTML + "0";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML !== "x") {
+			x.innerHTML = x.innerHTML + "0";
+		}
+	} else {
+		if (y.innerHTML !== "y") {
+			y.innerHTML = y.innerHTML + "0";
+		}
+	}
 });
 btn_1.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "1";
-        } else {
-            x.innerHTML = x.innerHTML + "1";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "1";
-        } else {
-            y.innerHTML = y.innerHTML + "1";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "1";
+		} else {
+			x.innerHTML = x.innerHTML + "1";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "1";
+		} else {
+			y.innerHTML = y.innerHTML + "1";
+		}
+	}
 });
 btn_2.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "2";
-        } else {
-            x.innerHTML = x.innerHTML + "2";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "2";
-        } else {
-            y.innerHTML = y.innerHTML + "2";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "2";
+		} else {
+			x.innerHTML = x.innerHTML + "2";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "2";
+		} else {
+			y.innerHTML = y.innerHTML + "2";
+		}
+	}
 });
 btn_3.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "3";
-        } else {
-            x.innerHTML = x.innerHTML + "3";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "3";
-        } else {
-            y.innerHTML = y.innerHTML + "3";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "3";
+		} else {
+			x.innerHTML = x.innerHTML + "3";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "3";
+		} else {
+			y.innerHTML = y.innerHTML + "3";
+		}
+	}
 });
 btn_4.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "4";
-        } else {
-            x.innerHTML = x.innerHTML + "4";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "4";
-        } else {
-            y.innerHTML = y.innerHTML + "4";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "4";
+		} else {
+			x.innerHTML = x.innerHTML + "4";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "4";
+		} else {
+			y.innerHTML = y.innerHTML + "4";
+		}
+	}
 });
 btn_5.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "5";
-        } else {
-            x.innerHTML = x.innerHTML + "5";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "5";
-        } else {
-            y.innerHTML = y.innerHTML + "5";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "5";
+		} else {
+			x.innerHTML = x.innerHTML + "5";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "5";
+		} else {
+			y.innerHTML = y.innerHTML + "5";
+		}
+	}
 });
 btn_6.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "6";
-        } else {
-            x.innerHTML = x.innerHTML + "6";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "6";
-        } else {
-            y.innerHTML = y.innerHTML + "6";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "6";
+		} else {
+			x.innerHTML = x.innerHTML + "6";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "6";
+		} else {
+			y.innerHTML = y.innerHTML + "6";
+		}
+	}
 });
 btn_7.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "7";
-        } else {
-            x.innerHTML = x.innerHTML + "7";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "7";
-        } else {
-            y.innerHTML = y.innerHTML + "7";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "7";
+		} else {
+			x.innerHTML = x.innerHTML + "7";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "7";
+		} else {
+			y.innerHTML = y.innerHTML + "7";
+		}
+	}
 });
 // btn_8.addEventListener("click", function () {
 //   if (select === "x") {
@@ -168,105 +170,128 @@ btn_7.addEventListener("click", function () {
 //   }
 // });
 btn_8.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "8";
-        } else {
-            x.innerHTML = x.innerHTML + "8";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "9";
-        } else {
-            y.innerHTML = y.innerHTML + "8";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "8";
+		} else {
+			x.innerHTML = x.innerHTML + "8";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "8";
+		} else {
+			y.innerHTML = y.innerHTML + "8";
+		}
+	}
 });
 btn_9.addEventListener("click", function () {
-    if (select === "x") {
-        if (x.innerHTML === "x") {
-            x.innerHTML = "9";
-        } else {
-            x.innerHTML = x.innerHTML + "9";
-        }
-    } else {
-        if (y.innerHTML === "y") {
-            y.innerHTML = "9";
-        } else {
-            y.innerHTML = y.innerHTML + "9";
-        }
-    }
+	if (select === "x") {
+		if (x.innerHTML === "x") {
+			x.innerHTML = "9";
+		} else {
+			x.innerHTML = x.innerHTML + "9";
+		}
+	} else {
+		if (y.innerHTML === "y") {
+			y.innerHTML = "9";
+		} else {
+			y.innerHTML = y.innerHTML + "9";
+		}
+	}
 });
 
-// +
+// =
 btn_eq.addEventListener("click", function () {
-    let x = Number(document.querySelector(".x").innerHTML);
-    let y = Number(document.querySelector(".y").innerHTML);
+	let x = Number(document.querySelector(".x").innerHTML);
+	let y = Number(document.querySelector(".y").innerHTML);
 
-    switch (type) {
-        case "+":
-            z = x + y;
-            break;
-        case "-":
-            z = x - y;
-            break;
-        case "*":
-            z = x * y;
-            break;
-        case "/":
-            z = x / y;
-            break;
-        case "%":
-            z = x % y;
-    }
+	switch (type) {
+		case "+":
+			z = x + y;
+			break;
+		case "-":
+			z = x - y;
+			break;
+		case "*":
+			z = x * y;
+			break;
+		case "/":
+			z = x / y;
+			break;
+		case "%":
+			z = x % y;
+	}
 
-    document.querySelector(".z").innerHTML = z;
-    document.querySelector(".x").style.background = "#A8AAFF";
-    document.querySelector(".y").style.background = "transparent";
+	document.querySelector(".z").innerHTML = z;
+	document.querySelector(".x").style.background = "#A8AAFF";
+	document.querySelector(".y").style.background = "transparent";
 
-    document.querySelector(".x").innerHTML = "x";
-    document.querySelector(".y").innerHTML = "y";
+	document.querySelector(".x").innerHTML = "x";
+	document.querySelector(".y").innerHTML = "y";
 
-    select = "x";
-
+	select = "x";
 });
 // +
 btn_plus.addEventListener("click", function () {
-    document.querySelector(".plus").innerHTML = "+";
-    x.style.background = "transparent";
-    y.style.background = "#A8AAFF";
-    select = "y";
-    type = "+";
+	document.querySelector(".plus").innerHTML = "+";
+	x.style.background = "transparent";
+	y.style.background = "#A8AAFF";
+	select = "y";
+	type = "+";
 });
 // -
 btn_minus.addEventListener("click", function () {
-    document.querySelector(".plus").innerHTML = "-";
-    x.style.background = "transparent";
-    y.style.background = "#A8AAFF";
-    select = "y";
-    type = "-";
+	document.querySelector(".plus").innerHTML = "-";
+	x.style.background = "transparent";
+	y.style.background = "#A8AAFF";
+	select = "y";
+	type = "-";
 });
 // *
 btn_multi.addEventListener("click", function () {
-    document.querySelector(".plus").innerHTML = "*";
-    x.style.background = "transparent";
-    y.style.background = "#A8AAFF";
-    select = "y";
-    type = "*";
+	document.querySelector(".plus").innerHTML = "*";
+	x.style.background = "transparent";
+	y.style.background = "#A8AAFF";
+	select = "y";
+	type = "*";
 });
 // /(割る)
 btn_div.addEventListener("click", function () {
-    document.querySelector(".plus").innerHTML = "/";
-    x.style.background = "transparent";
-    y.style.background = "#A8AAFF";
-    select = "y";
-    type = "/";
+	document.querySelector(".plus").innerHTML = "/";
+	x.style.background = "transparent";
+	y.style.background = "#A8AAFF";
+	select = "y";
+	type = "/";
 });
-btn_point.addEventListener("click", function () {
-    document.querySelector(".point").innerHTML = "%";
-    x.style.background = "transparent";
-    y.style.background = "#A8AAFF";
-    select = "y";
-    type = "%";
-    console.log(btn_point);
-})
+// C: clear
+btn_clear.addEventListener("click", function () {
+	x.style.background = "#A8AAFF";
+	y.style.background = "transparent";
+	select = "x";
+	type = "+";
+	document.querySelector(".x").innerHTML = "x";
+	document.querySelector(".plus").innerHTML = "+";
+	document.querySelector(".y").innerHTML = "y";
+	document.querySelector(".z").innerHTML = "z";
+});
+// ±
+btn_plus_minus.addEventListener("click", function () {
+	if (select === "x") {
+		if (x.innerHTML !== "x") {
+			x.innerHTML = x.innerHTML * -1;
+		}
+	}
+	if (select === "y") {
+		if (y.innerHTML !== "y") {
+			y.innerHTML = y.innerHTML * -1;
+		}
+	}
+});
+// %
+btn_percent.addEventListener("click", function () {
+	document.querySelector(".plus").innerHTML = "%";
+	x.style.background = "transparent";
+	y.style.background = "#A8AAFF";
+	select = "y";
+	type = "%";
+});
